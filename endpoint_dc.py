bcd125c95da97a55d627074354bae4d9d71aa3ba
Bikael <mmacdoug02@gmail.com>
2025-09-03 13:21:47 -0400

    added find cheapest item function

--- endpoint_dc.py
+++ endpoint_dc.py
@@ -3,7 +3,6 @@ import pprint, json
 
 class EndpointDC:
     
-
     def __init__(self, postal_code):
         self.postal_code = postal_code
 
@@ -23,8 +22,9 @@ class EndpointDC:
 
         for item in items:
             filtered_item = {
-                "name" : item["name"],
+                "name" : item["name"], 
                 "current_price" : item["current_price"],
+                "merchant_name" : item["merchant_name"],
                 "clean_image_url" : item["clean_image_url"],
                 "clipping_image_url" : item["clipping_image_url"]
             }
@@ -34,13 +34,32 @@ class EndpointDC:
             json.dump(filtered_items, file, indent=4)
             
         pprint.pprint(filtered_items)
-    
-    
 
+        return filtered_items
 
-endpoint = EndpointDC("K2T1J1")
-data = endpoint.search_item("eggs")
-items = endpoint.process_json(data)
+    def find_cheapest_item(self, items): 
         
+        cheapest_items = [items[0]]
+        
+        for item in items:
+            print(cheapest_items[0]["current_price"])
+            if item["current_price"] != None and item["current_price"] < cheapest_items[0]["current_price"]:
+                cheapest_items = [item]
+            elif cheapest_items[0]["current_price"] == item["current_price"]:
+                cheapest_items.append(item)
+
+
+        print(cheapest_items)
+        return cheapest_items
+    
+    
+        
+
+            
+        
+
+    
+    
+
         
 
