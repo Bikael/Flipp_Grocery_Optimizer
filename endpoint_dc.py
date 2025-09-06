75a4524def878bcfc63b7e7bdacdecfc5a4040ac
Bikael <mmacdoug02@gmail.com>
2025-09-06 17:40:07 -0400

    reorganizing structure of project

--- endpoint_dc.py
+++ endpoint_dc.py
@@ -7,55 +7,19 @@ class EndpointDC:
     def __init__(self, postal_code, grocery_list):
         self.postal_code = postal_code
         self.grocery_list = grocery_list
-        
-
-    def search_item(self, item):
-        # get endpoint json data from url
-        url = f"https://backflipp.wishabi.com/flipp/items/search?locale=en-ca&postal_code={self.postal_code}&q={item}"
-        response = requests.get(url)
-        response.raise_for_status()
-
-        # temporarily put json data into a file 
-        webpage_data = response.json()
-        return webpage_data
     
     def process_json(self, json_data):
         filtered_items = []
-        items = json_data["items"]
 
-        # Filter out neccessary fields 
-        for item in items:
+        # Filter out neccessary fields, Subject to change depending on what fields are required
+        for item in json_data:
             filtered_item = {
                 "name" : item["name"], 
-                "current_price" : item["current_price"],
-                "merchant_name" : item["merchant_name"],
-                "clean_image_url" : item["clean_image_url"],
-                "clipping_image_url" : item["clipping_image_url"]
+                "price" : item["price"]
             }
             filtered_items.append(filtered_item)
 
         return filtered_items
-
-    def find_cheapest_item(self, items): 
-        cheapest_items = [items[0]]
-        # Find cheapest flyers for respective item
-        for item in items:
-            if item["current_price"] != None and item["current_price"] < cheapest_items[0]["current_price"]:
-                cheapest_items = [item]
-            elif cheapest_items[0]["current_price"] == item["current_price"]:
-                cheapest_items.append(item)
-        return cheapest_items
-    
-
-    def create_cheapest_list(self):
-        final_list = []
-        for item in self.grocery_list:
-            data = self.search_item(item)
-            items = self.process_json(data)
-            cheapest_item = self.find_cheapest_item(items)
-            final_list.append(cheapest_item)
-        
-        return final_list
     
     def get_flyers(self, store):
         # get endpoint json data from url
@@ -82,20 +46,8 @@ class EndpointDC:
             response = requests.get(url)
             response.raise_for_status()
             webpage_data = response.json()
-            
-            flyer_data.append(webpage_data["items"])
+
+            flyer_data += webpage_data["items"]
         
         json.dumps(flyer_data, indent=4)
-        return flyer_data
-    
-
-    
-
-            
-        
-
-    
-    
-
-        
-
+        return flyer_data    
