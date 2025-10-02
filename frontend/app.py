3be759bc88630ee07905b2dba567b758c9b95331
Bikael <mmacdoug02@gmail.com>
2025-10-02 17:51:31 -0400

    moved app.py to backend

--- frontend/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-import requests
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
